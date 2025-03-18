966d70daa824004c964139e9f6fb81e37df5d027
dong1hyun <limd1238@naver.com>
2025-03-18 21:12:38 +0900

    accountInfo 구현

--- Frontend/screens/PlaceDetailScreen.tsx
+++ Frontend/screens/PlaceDetailScreen.tsx
@@ -4,7 +4,7 @@ import { View } from "react-native";
 import { HomeStackParamList, PostResponseType } from "../lib/type";
 import { deleteData, getData } from "../util/fetch";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
-import FontAwesome6Icon from "react-native-vector-icons/FontAwesome6";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 import { getRelativeTime } from "../util/date";
 import StaticMap from "../components/molecules/StaticMap";
 import Spinner from "../components/atoms/SpinLoading";
@@ -46,10 +46,9 @@ export default function PlaceDetailScreen({ route, navigation }: Props) {
 
     const deletePlace = async () => {
         try {
-            console.log("시작")
             setDeleteLoading(true);
             await deleteData(`${API_URL}/place/${id}`);
-            console.log("노에러")
+            setModalVisible(false);
             queryClient.invalidateQueries({queryKey: ['places']});
             navigation.navigate("Home");
         } catch(error) {
@@ -65,7 +64,7 @@ export default function PlaceDetailScreen({ route, navigation }: Props) {
                 <Image source={{ uri: data?.photoUrl }} style={styles.image} resizeMode="cover" />
                 <View style={styles.contentContainer}>
                     <View style={styles.uerContainer}>
-                        <FontAwesome6Icon style={styles.userIcon} name="circle-user" />
+                        <FontAwesome style={styles.userIcon} name="user-circle-o" />
                         <Text>{data?.nickName}</Text>
                     </View>
                     <Text style={styles.title}>{data?.title}</Text>
@@ -82,7 +81,7 @@ export default function PlaceDetailScreen({ route, navigation }: Props) {
             </ScrollView>
             {
                 nickName === data?.nickName &&
-                <EditButtons id={id} data={data} setModalVisible={setModalVisible} />
+                <EditButtons data={data} setModalVisible={setModalVisible} />
             }
             <ModalContainer modalVisible={modalVisible} setModalVisible={setModalVisible}>
                 <View style={styles.modalContainer}>
@@ -104,7 +103,6 @@ const styles = StyleSheet.create({
     },
     userIcon: {
         fontSize: 36,
-        color: "gray"
     },
     uerContainer: {
         flexDirection: 'row',
